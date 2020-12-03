9163c20f46d7d24e34c7729e2def9c0b03723edd
william97-su <75348924+william97-su@users.noreply.github.com>
2020-12-03 12:25:11 -0300

    Update proyect1.java

--- proyect1.java
+++ proyect1.java
@@ -1,4 +1,12 @@
 programa Test
+    int a 
+    int b
+    int suma
     mostrar: "Hola mundo"
     mostrar: "chau"
+    mostrar: "suma de a y b"
+    ingresar: a
+    ingresar: b
+    suma = a + b
+    mostar = "la suma es :" + suma
 fin
